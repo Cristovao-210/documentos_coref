b94f990713aa0a385256b5e3b0239f58e48ad8b1
Formularios <formulariosdapdgp@gmail.com>
2023-05-26 16:07:57 -0300

    Estabelecendo conexão com o fork

--- main.py
+++ main.py
@@ -6,3 +6,4 @@ st.subheader("ATOS DE FUNÇÃO")
 documento_selecionado = st.sidebar.selectbox("Tipo de Documento", lista_de_documentos)
 if documento_selecionado == "Ato":
     setor_responsavel = st.sidebar.selectbox("Setor Responsável", lista_de_setores)
+    
