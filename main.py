2ba7f1d29e567ca3ad27d9edbccd7dffedcbd282
Formularios <formulariosdapdgp@gmail.com>
2023-05-27 16:22:53 -0300

    Alteração de título

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from auxiliares.var_globais import *
 
-st.subheader("ATOS DE FUNÇÃO")
+st.subheader("GERADOR DE DOCUMENTOS OFICIAIS")
 
 documento_selecionado = st.sidebar.selectbox("Tipo de Documento", lista_de_documentos)
 if documento_selecionado == "Ato":
