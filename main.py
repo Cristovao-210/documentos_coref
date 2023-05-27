b99c7b95eaf126600b2ca5d2d567e7d31163cc36
Formularios <formulariosdapdgp@gmail.com>
2023-05-27 16:27:01 -0300

    Incluíndo ideia de gerar word para publicação

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ st.subheader("GERADOR DE DOCUMENTOS OFICIAIS")
 
 documento_selecionado = st.sidebar.selectbox("Tipo de Documento", lista_de_documentos)
 if documento_selecionado == "Ato":
-    setor_responsavel = st.sidebar.selectbox("Setor Responsável", lista_de_setores)
+    acao_com_documento = st.sidebar.selectbox("Setor Responsável", ("", "Gerar Ato", "Preparar para Publicação"))
     
