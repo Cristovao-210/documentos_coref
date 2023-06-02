784d9058682c7cd51035d632d012a104c245d3d3
Márcio Cristóvão <cristovaocodigos@gmail.com>
2023-06-02 16:25:42 -0300

    Teste commit pc novo

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ st.subheader("GERADOR DE DOCUMENTOS OFICIAIS")
 documento_selecionado = st.sidebar.selectbox("Tipo de Documento", lista_de_documentos)
 if documento_selecionado == "Ato":
     acao_com_documento = st.sidebar.selectbox("Setor Responsável", ("", "Gerar Ato", "Preparar para Publicação"))
+    # teste commit
     
