6aae3353834a0b7484f4dd60169ff5d5e15a27e4
Cristovao-210 <57817099+Cristovao-210@users.noreply.github.com>
2024-06-28 09:26:59 -0300

    Update compor_arquivo_atos.py
    Atualização nos preâmbulos conforme solicitação da Bia em 26/06/2024

--- documentos/ato/compor_arquivo_atos.py
+++ documentos/ato/compor_arquivo_atos.py
@@ -59,24 +59,22 @@ preambulo = {
         'html': '''
             <p style="font-size:12pt;font-family:Calibri;text-indent:25mm;text-align:justify;word-wrap:normal;margin:6pt;">{0}</p> ''',
         'txt_dgp': '''A DECANA DE GESTÃO DE PESSOAS DA UNIVERSIDADE DE BRASÍLIA, 
-                    no uso de suas atribuições legais, conferidas pelo Ato da Reitoria nº .72,
-                    publicado no DOU n. 13, de 20 de janeiro de 2021, seção 2, página 24,
-                    e de acordo com a competência que lhe foi delegada por meio do Ato da Reitoria n. 304,
-                    de 23 de março de 2017, publicado no DOU n. 58, de 24 de março de 2017 
-                    e à vista do constante no Processo Eletrônico nº {0},''',
-        'txt_dgp_substituto': '''A DECANA DE GESTÃO DE PESSOAS EM EXERCÍCIO DA UNIVERSIDADE DE BRASÍLIA, 
-                                 no uso de suas atribuições legais, conferidas pelo Ato da Reitoria nº 89 
-                                 publicado no DOU nº 16, de 25 janeiro de 2021, seção 2, página 30 e de acordo 
-                                 com a competência que lhe foi delegada por meio do Ato da Reitoria n. 304, de 23 de março de 2017, 
-                                 publicado no DOU n. 58, de 24 de março de 2017 e à vista do constante no Processo Eletrônico nº {0},''',
-        'txt_dgp_dap': '''O DIRETOR DE ADMINISTRAÇÃO DE PESSOAS DO DECANATO DE GESTÃO DE PESSOAS DA UNIVERSIDADE DE BRASÍLIA,
-                       no uso de suas atribuições legais e de acordo com a competência que lhe foi delegada
-                       por meio do Ato do Decanato de Gestão de Pessoas n. 1.593, de 03 de abril de 2017,
-                       publicado no DOU n. 66, de 05 de abril de 2017 e à vista do constante no Processo Eletrônico nº {0},''',
+                        no uso de suas atribuições legais, conferidas pelo Ato da Reitoria nº 72, 
+                        publicado no DOU nº 13, de 20 de janeiro de 2021, seção 2, página 24, 
+                        e de acordo com a competência que lhe foi delegada por meio do Ato da Reitoria nº 304, 
+                        de 23 de março de 2017, publicado no DOU nº 58, de 24 de março de 2017 e à vista do constante no Processo Eletrônico nº {0},''',
+        'txt_dgp_substituto': '''A DECANA DE GESTÃO DE PESSOAS EM EXERCÍCIO DA UNIVERSIDADE DE BRASÍLIA, no uso de suas atribuições legais, conferidas pelo
+                                Ato da Reitoria nº 89 publicado no DOU nº 16, de 25 janeiro de 2021, seção 2, página 30 e de acordo com a competência que lhe 
+                                foi delegada por meio do Ato da Reitoria nº 304, de 23 de março de 2017, publicado no DOU nº 58, de 24 de março de 2017 
+                                e à vista do constante no Processo Eletrônico nº {0},''',
+        'txt_dgp_dap': '''O DIRETOR DE ADMINISTRAÇÃO DE PESSOAS DO DECANATO DE GESTÃO DE PESSOAS DA UNIVERSIDADE DE BRASÍLIA, 
+                          no uso de suas atribuições legais e de acordo com a competência que lhe foi delegada por meio do 
+                          Ato do Decanato de Gestão de Pessoas nº 1.593, de 03 de abril de 2017, publicado no DOU nº 66, de 05 de abril de 2017 
+                          e à vista do constante no Processo Eletrônico nº {0},''',
         'txt_dgp_dap_susbstituto': '''O(A) DIRETOR(A) DE ADMINISTRAÇÃO DE PESSOAS SUSTITUTO(A) DO DECANATO DE GESTÃO DE PESSOAS DA UNIVERSIDADE DE BRASÍLIA,
-                       no uso de suas atribuições legais e de acordo com a competência que lhe foi delegada
-                       por meio do Ato do Decanato de Gestão de Pessoas n. 1.593, de 03 de abril de 2017,
-                       publicado no DOU n. 66, de 05 de abril de 2017 e à vista do constante no Processo Eletrônico nº {0},''',               
+                                      no uso de suas atribuições legais e de acordo com a competência que lhe foi delegada por meio do Ato do Decanato de 
+                                      Gestão de Pessoas nº 1.593, de 03 de abril de 2017, publicado no DOU nº 66, de 05 de abril de 2017
+                                      e à vista do constante no Processo Eletrônico nº {0},''',               
         'txt_reitoria': 'A REITORA DA UNIVERSIDADE DE BRASÍLIA, no uso de suas atribuições e considerando o constante no Processo nº {0},',
         'txt_reitoria_substituto': 'O VICE-REITOR, NO EXERCÍCIO DA REITORIA DA UNIVERSIDADE DE BRASÍLIA, no uso de suas atribuições estatutárias e considerando o constante no Processo nº {0}',
         'txt_erro': 'ERRO: Nenhum Tipo de Setor Responsável selecionado.'
